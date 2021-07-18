aa128d1d09d17708b7a9b0b8c2f59aaa2f30dea5
Criptograma-1 <andresa.info@gmail.com>
2021-07-18 02:36:23 -0300

    Add new sections

--- src/index.js
+++ src/index.js
@@ -22,3 +22,32 @@ function addItem() {
     );
   }
 }
+var target_date = new Date("august 1, 2021").getTime();
+var dias, horas, minutos, segundos;
+//var regressiva = document.getElementById("regressiva");
+
+setInterval(function () {
+  var current_date = new Date().getTime();
+  var segundos_f = (target_date - current_date) / 1000;
+  
+  dias = parseInt(segundos_f / 86400);
+  segundos_f = segundos_f % 86400;
+    
+    horas = parseInt(segundos_f / 3600);
+    segundos_f = segundos_f % 3600;
+    
+    minutos = parseInt(segundos_f / 60);
+    segundos = parseInt(segundos_f % 60);
+
+    document.getElementById('dia').value = dias;
+document.getElementById('hora').value = horas;
+document.getElementById('minuto').value = minutos;
+document.getElementById('segundo').value = segundos;
+  
+
+}, 1000);
+
+
+function subir() {
+    window.scrollTo(0, 0);
+}
