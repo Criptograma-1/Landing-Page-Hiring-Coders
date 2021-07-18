51a506a6bb8bd06b8083f89e9a380908bec84944
Criptograma-1 <andresa.info@gmail.com>
2021-07-18 12:05:34 -0300

    Add comments

--- src/index.js
+++ src/index.js
@@ -1,17 +1,14 @@
 function addItem() {
-
-  // Pegando o valor do campo email
+  //Pegando o valor do campo email
   let email = document.getElementById("email").value;
-
-  // Criando uma string com o objeto a ser adicionado no localstorage
+  //Criando uma string com o objeto a ser adicionado no localstorage
   const dataObj = JSON.stringify({ email });
-
   //Testando se é o primeiro item a ser adicionado.
   if (localStorage.getItem('items') === null) {
-    // Adicionando um array com um objeto no localstorage
+    //Adicionando um array com um objeto no localstorage
     localStorage.setItem('items', JSON.stringify([dataObj]));
   } else {
-    // Copiando o array existente no localstorage e adicionando o novo objeto ao final.
+    //Copiando o array existente no localstorage e adicionando o novo objeto ao final.
     localStorage.setItem(
       'items',
       // O JSON.parse transforma a string em JSON novamente, o inverso do JSON.strigify
@@ -21,32 +18,37 @@ function addItem() {
       ])
     );
   }
-}
+};
+
+//Data final
 let target_date = new Date("august 1, 2021").getTime();
 let dias, horas, minutos, segundos;
 
 setInterval(function () {
+  //Data atual
   let current_date = new Date().getTime();
+  //Quantos segundos faltam para a data final
   let segundos_f = (target_date - current_date) / 1000;
-  
+  //Conversão de segundos para dias
   dias = parseInt(segundos_f / 86400);
   segundos_f = segundos_f % 86400;
-    
-    horas = parseInt(segundos_f / 3600);
-    segundos_f = segundos_f % 3600;
-    
-    minutos = parseInt(segundos_f / 60);
-    segundos = parseInt(segundos_f % 60);
+  //Conversão de segundos para horas
+  horas = parseInt(segundos_f / 3600);
+  segundos_f = segundos_f % 3600;
+  //Conversão de segundos para minutos
+  minutos = parseInt(segundos_f / 60);
+  segundos = parseInt(segundos_f % 60);
+  //Atribuindo resultados ao form
+  document.getElementById('dia').value = dias;
+  document.getElementById('hora').value = horas;
+  document.getElementById('minuto').value = minutos;
+  document.getElementById('segundo').value = segundos;
 
-    document.getElementById('dia').value = dias;
-document.getElementById('hora').value = horas;
-document.getElementById('minuto').value = minutos;
-document.getElementById('segundo').value = segundos;
-  
 
 }, 1000);
 
 
-function subir() {
-    window.scrollTo(0, 0);
+function upToTop() {
+  //Sobe para a o topo da tela
+  window.scrollTo(0, 0);
 }
